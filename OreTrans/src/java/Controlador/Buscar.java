5b2d4436a25eecc402ffd2e084c4b6336029f26f
Jonaiker2198 <65262093+Jonaiker2198@users.noreply.github.com>
2021-11-17 20:26:40 -0500

    Update Buscar.java

--- OreTrans/src/java/Controlador/Buscar.java
+++ OreTrans/src/java/Controlador/Buscar.java
@@ -15,10 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author javie
- */
 public class Buscar extends HttpServlet {
 
     PersonaDAO dao = new PersonaDAO();
